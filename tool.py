02ccdb8bc0f71f10b870f0f2934927413e1bc4c8
aqudei <fdeu.aalmine@gmail.com>
2023-01-28 16:39:40 +0800

    sdsd

--- tool.py
+++ tool.py
@@ -4,11 +4,8 @@ import re
 
 re_title = re.compile(r'<title>.+</title>')
 
-if __name__ == "__main__":
-    parser = ArgumentParser()
 
-    # Set title to Jeff
-
+def change_tile():
     for item in os.listdir("."):
         _, ext = os.path.splitext(item)
         if not '.html' in ext.lower():
@@ -22,3 +19,22 @@ if __name__ == "__main__":
                 newhtml = html.replace(title.group(0), newtitle)
                 with open("./" + item, 'wt', encoding='utf8') as outfile:
                     outfile.write(newhtml)
+
+
+def replace_text(origninal, replacement):
+    for item in os.listdir("."):
+        _, ext = os.path.splitext(item)
+        if not '.html' in ext.lower():
+            continue
+
+        with open("./" + item, 'rt', encoding='utf8') as infile:
+            html = infile.read()
+            newhtml = html.replace(origninal, replacement)
+            with open("./" + item, 'wt', encoding='utf8') as outfile:
+                outfile.write(newhtml)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+
+    replace_text("HOPain Tools", "D7 OSINT Tools")
